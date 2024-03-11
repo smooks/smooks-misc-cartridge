01b567c415aeb33a31860790131bbd0c8a1e6fbb
Claude Mamo <823038+claudemamo@users.noreply.github.com>
2024-03-11 10:25:45 +0100

    chore: prepare for development (#71)
    fix: solve errors caused by changes in Smooks dependencies
    
    docs: remove inherited obsolete ISSUE_TEMPLATE directory

--- src/test/java/org/smooks/cartridges/cdres/trans/BaseTransUnitsTest.java
+++ src/test/java/org/smooks/cartridges/cdres/trans/BaseTransUnitsTest.java
@@ -6,35 +6,35 @@
  * %%
  * Licensed under the terms of the Apache License Version 2.0, or
  * the GNU Lesser General Public License version 3.0 or later.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-or-later
- * 
+ *
  * ======================================================================
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * ======================================================================
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -52,8 +52,8 @@ import org.smooks.engine.injector.Scope;
 import org.smooks.engine.lifecycle.PostConstructLifecyclePhase;
 import org.smooks.engine.lookup.LifecycleManagerLookup;
 import org.smooks.engine.resource.config.DefaultResourceConfig;
+import org.smooks.support.XmlUtils;
 import org.smooks.tck.MockApplicationContext;
-import org.smooks.support.XmlUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -72,7 +72,7 @@ public class BaseTransUnitsTest {
         registry = applicationContext.getRegistry();
         lifecycleManager = registry.lookup(new LifecycleManagerLookup());
     }
-    
+
     @Test
     public void test_RenameAttributeTU() {
         RenameAttributeTU tu = new RenameAttributeTU();
@@ -84,7 +84,7 @@ public class BaseTransUnitsTest {
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
 
         Document doc = parseCPResource("/testpage1.html");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         tu.visitAfter(body, null);
         assertEquals("Default overwrite protection failed.", "value2", body.getAttribute("attrib2"));
 
@@ -104,7 +104,7 @@ public class BaseTransUnitsTest {
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
 
         Document doc = parseCPResource("/testpage1.html");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         assertTrue("XPath failed - test corrupted.", body.hasAttribute("attrib1"));
         tu.visitAfter(body, null);
         assertFalse("Failed to remove target attribute.", body.hasAttribute("attrib1"));
@@ -119,48 +119,48 @@ public class BaseTransUnitsTest {
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
 
         Document doc = parseCPResource("/testpage1.html");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         tu.visitAfter(body, null);
-        assertNull("Failed to rename target element.", XmlUtil.getNode(doc, "/html/body"));
-        assertNotNull("Failed to rename target element.", XmlUtil.getNode(doc, "/html/head"));
+        assertNull("Failed to rename target element.", XmlUtils.getNode(doc, "/html/body"));
+        assertNotNull("Failed to rename target element.", XmlUtils.getNode(doc, "/html/head"));
     }
 
     @Test
     public void test_RenameElementTU_root_element() {
         Document doc = parseCPResource("/testpage1.html");
         ResourceConfig resourceConfig = new DefaultResourceConfig("body", new Properties(), "device", "xxx");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         RenameElementTU tu;
 
         resourceConfig.setParameter("replacementElement", "head");
         tu = new RenameElementTU();
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
-        
+
         tu.visitAfter(body, null);
-        assertNull("Failed to rename target element.", XmlUtil.getNode(doc, "/html/body"));
-        assertNotNull("Failed to rename target element.", XmlUtil.getNode(doc, "/html/head"));
+        assertNull("Failed to rename target element.", XmlUtils.getNode(doc, "/html/body"));
+        assertNotNull("Failed to rename target element.", XmlUtils.getNode(doc, "/html/head"));
     }
 
     @Test
     public void test_RemoveElementTU() {
         Document doc = parseCPResource("/testpage1.html");
         ResourceConfig resourceConfig = new DefaultResourceConfig("body", new Properties(), "device", "xxx");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         RemoveElementTU tu = new RemoveElementTU();
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
-        
+
         tu.visitAfter(body, null);
-        assertNull("Failed to remove target element.", XmlUtil.getNode(doc, "/html/body"));
+        assertNull("Failed to remove target element.", XmlUtils.getNode(doc, "/html/body"));
     }
 
     @Test
     public void test_RemoveElementTU_root_element() {
         Document doc = parseCPResource("/testpage1.html");
         ResourceConfig resourceConfig = new DefaultResourceConfig("html", new Properties(), "xxx");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         RemoveElementTU tu = new RemoveElementTU();
         lifecycleManager.applyPhase(tu, new PostConstructLifecyclePhase(new Scope(registry, resourceConfig, tu)));
-        
+
         // So remove the root element...
         tu.visitAfter(doc.getDocumentElement(), null);
         assertEquals("Failed to remove root element.", body, doc.getDocumentElement());
@@ -174,7 +174,7 @@ public class BaseTransUnitsTest {
     public void test_SetAttributeTU() {
         Document doc = parseCPResource("/testpage1.html");
         ResourceConfig resourceConfig = new DefaultResourceConfig("body", new Properties(), "device", "xxx");
-        Element body = (Element) XmlUtil.getNode(doc, "/html/body");
+        Element body = (Element) XmlUtils.getNode(doc, "/html/body");
         SetAttributeTU tu = new SetAttributeTU();
 
         resourceConfig.setParameter("attributeName", "attrib1");
@@ -192,7 +192,7 @@ public class BaseTransUnitsTest {
 
     public Document parseCPResource(String classpath) {
         try {
-            return XmlUtil.parseStream(getClass().getResourceAsStream(classpath), XmlUtil.VALIDATION_TYPE.NONE, true);
+            return XmlUtils.parseStream(getClass().getResourceAsStream(classpath), XmlUtils.VALIDATION_TYPE.NONE, true);
         } catch (Exception e) {
             e.printStackTrace();
             fail(e.getMessage());
